0858cd57e65b2bdf832772ae4903af7a0622a810
Matthew Brush <mbrush@codebrainz.ca>
2017-07-31 22:47:24 -0700

    Reorder passes a bit

--- Pop/Main.cpp
+++ Pop/Main.cpp
@@ -6,12 +6,12 @@ using namespace Pop;
 
 int main() {
   Compiler cmp;
-  cmp.parse_file("test.pop"); // 0
-  cmp.link_parents(true);     // 1
-  cmp.define_symbols();       // 2
-  cmp.resolve_symbols();      // 3
-  cmp.patch_locations(true);  // 4
-  cmp.validate();             // 5
+  cmp.parse_file("test.pop");
+  cmp.patch_locations(true);
+  cmp.link_parents(true);
+  cmp.define_symbols();
+  cmp.resolve_symbols();
+  cmp.validate();
   cmp.generate_dot(std::cout);
   assert(cmp.report_diagnostics(std::cerr) == 0);
   return 0;
